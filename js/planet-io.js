31113dcfc3f502b0d02291d327a6965721441090
Daniel <Daniel@Daniels-MacBook-Pro.local>
2013-01-04 18:15:47 +0100

    Part 2 of major cleanup.

---
+++ js/planet-io.js
@@ -0,0 +1,223 @@
+/***********************************************************************
+ * Viewport
+ **********************************************************************/
+
+Planets.Viewport = function(game, w, h) {
+	this.game = game;
+
+	this.w = w;						//logical width
+	this.h = h;						//logical height
+
+	this.vw = $(window).width();	//viewport height
+	this.vh = $(window).height();	//viewport width
+
+	$('body').css('background-color', Planets.const.viewportBackgroundColor);
+
+	this.jq_bgcanvas = $('<canvas></canvas>')
+	.width(this.vw)
+	.height(this.vh)
+	.appendTo($('body'));
+
+	this.bgcanvas = this.jq_bgcanvas.get(0);
+	this.bgcanvas.width = this.vw;
+	this.bgcanvas.height = this.vh;
+	this.bgcontext = this.bgcanvas.getContext('2d');
+
+	this.jq_canvas = $('<canvas></canvas>')
+	.width(this.vw)
+	.height(this.vh)
+	.appendTo($('body'));
+	
+	this.canvas = this.jq_canvas.get(0);
+	this.canvas.width = this.vw;
+	this.canvas.height = this.vh;
+	this.context = this.canvas.getContext('2d');
+
+	this.offset = {x: 0, y: 0};
+
+	this.moveSpeed = Planets.const.scrollSpeed;		// Screen movement speed
+	this.moveCorner = Planets.const.hotCornerSize;	// Hot corner size
+
+	// this.bgUpdated = false;
+}
+
+Planets.Viewport.prototype.writeDebug = function(update, render, full, fps, pfps) {
+	this.context.font = "10pt Lucida Console";
+	this.context.fillStyle = "#ffffff";
+	this.context.fillText("Update: " + update, 10 - this.offset.x, 20 - this.offset.y);
+	this.context.fillText("Render: " + render, 10 - this.offset.x, 30 - this.offset.y);
+	this.context.fillText("Combin: " + full,   10 - this.offset.x, 40 - this.offset.y);
+	this.context.fillText("PotFPS: " + pfps,   10 - this.offset.x, 50 - this.offset.y);
+	this.context.fillText("ReaFPS: " + fps,   10 - this.offset.x, 60 - this.offset.y);
+	this.context.fill();
+}
+
+Planets.Viewport.prototype.clear = function() {
+	if(this.game.bgLayer.isInvalid()) {
+		//if the background has been updated, clear the
+		//transformations, clear the canvas and apply
+		//the new offsets
+		this.context.setTransform(1, 0, 0, 1, 0, 0);
+		this.context.clearRect(0, 0, this.vw, this.vh);
+		this.context.translate(this.offset.x, this.offset.y);
+
+		this.bgcontext.setTransform(1, 0, 0, 1, 0, 0);
+		this.bgcontext.clearRect(0, 0, this.vw, this.vh);
+		this.bgcontext.translate(this.offset.x, this.offset.y);
+	} else {
+		//nothing has changed - clear the foreground canvas (with regards to
+		//the translated offsets)
+		this.context.clearRect(0, 0, this.vw - this.offset.x, this.vh - this.offset.y);
+	}
+}
+
+Planets.Viewport.prototype.circleVisible = function(x, y, r) {
+	if(x+r < this.offset.x || x-r > (this.vw - this.offset.x )) return false;
+	if(y+r < this.offset.y || y-r > (this.vh - this.offset.y )) return false;
+	return true;
+}
+
+Planets.Viewport.prototype.rectVisible = function(x1, y1, x2, y2) {
+	if(x2 < this.offset.x || x1 > (this.vw - this.offset.x )) return false;
+	if(y2 < this.offset.y || y1 > (this.vh - this.offset.y )) return false;
+	return true;
+}
+
+Planets.Viewport.prototype.handleInput = function(mouse, keyboard, touch) {
+	//this.bgUpdated = false;
+	this.handleMouse(mouse);
+	this.handleKeydown(keyboard);
+}
+
+// Handle viewport offset when mouse is in hot corners
+Planets.Viewport.prototype.handleMouse = function(mouse) {
+	if(mouse.absolute.y < this.moveCorner) 	
+		this.moveDown();
+	if(mouse.absolute.y > this.vh - this.moveCorner) 
+		this.moveUp();
+	if(mouse.absolute.x < this.moveCorner) 
+		this.moveLeft();
+	if(mouse.absolute.x > this.vw - this.moveCorner) 
+		this.moveRight();
+}
+
+// Handle viewport offset when arrow-keys are pressed
+Planets.Viewport.prototype.handleKeydown = function(keyboard) {
+	if(keyboard.keymap[Keys.UP]) 	this.moveUp();
+	if(keyboard.keymap[Keys.DOWN]) 	this.moveDown();
+	if(keyboard.keymap[Keys.LEFT])	this.moveLeft();
+	if(keyboard.keymap[Keys.RIGHT])	this.moveRight();
+}
+
+// Viewport movement methods
+Planets.Viewport.prototype.moveUp = function() {
+		this.game.bgLayer.invalidate();
+		this.offset.y -= this.moveSpeed;
+		if(this.offset.y < this.vh - this.h) 
+			this.offset.y = (this.vh - this.h);
+}
+
+Planets.Viewport.prototype.moveDown = function() {
+		this.game.bgLayer.invalidate();
+		this.offset.y += this.moveSpeed;
+		if(this.offset.y > 0)
+			this.offset.y = 0;
+}
+
+Planets.Viewport.prototype.moveLeft = function() {
+		this.game.bgLayer.invalidate();
+		this.offset.x += this.moveSpeed;
+		if(this.offset.x > 0)
+			this.offset.x = 0;
+}
+
+Planets.Viewport.prototype.moveRight = function() {
+		this.game.bgLayer.invalidate();
+		this.offset.x -= this.moveSpeed;
+		if(this.offset.x < this.vw - this.w)
+			this.offset.x = (this.vw - this.w);
+}
+
+/***********************************************************************
+ * Mouse
+ **********************************************************************/
+
+Planets.Mouse = function(game, viewport) {
+	this.game = game; 
+	this.viewport = viewport;
+
+	viewport.canvas.onmousemove = this.handler.bind(this);
+	viewport.jq_canvas.mousewheel(this.wheelHandler.bind(this));
+	viewport.jq_canvas.bind('mousedown', this.downHandler.bind(this));
+	viewport.jq_canvas.bind('mouseup', this.upHandler.bind(this));
+
+
+	//position including offset calculation
+	this.position = {x: 0, y: 0};
+	//absolute position (for fixed gui elements)
+	this.absolute = {x: 0, y: 0};
+
+
+	this.delta = 0;
+	this.left = viewport.jq_canvas.offset().left;
+	this.top  = viewport.jq_canvas.offset().top;
+	this.currentDown = null;	// Element on which the mousebutton was pressed down
+	this.down = false;
+}
+
+// Returns the delat and clears it (sets it to 0);
+Planets.Mouse.prototype.getClearDelta = function() {
+	var d = this.delta;
+	this.delta = 0;
+	return Math.round(d);
+}
+
+Planets.Mouse.prototype.handler = function(event) {
+	this.absolute.x = event.pageX - this.left;
+	this.absolute.y = event.pageY - this.top;
+	this.position.x = this.absolute.x - this.viewport.offset.x;
+	this.position.y = this.absolute.y - this.viewport.offset.y;
+}
+
+Planets.Mouse.prototype.wheelHandler = function(event, delta) {
+	event.preventDefault();
+	this.delta = delta;
+}
+
+Planets.Mouse.prototype.downHandler = function(event) {
+	this.currentDown = this.game.selected;
+	this.down = true;
+}
+
+Planets.Mouse.prototype.upHandler = function(event) {
+	if( this.currentDown != null && 
+		this.game.selected != null &&
+		this.currentDown != this.game.selected) 
+			this.currentDown.moveSelectedShips(this.game.selected, Fraction.Player);
+	
+	this.down = false;
+	this.currentDown = null;
+}
+
+/***********************************************************************
+ * Key bindings
+ **********************************************************************/
+
+Planets.Keymap = function(game, viewport) { 
+	this.keymap = new Array(128);
+	for(var i = 0; i < 128; i++)
+		this.keymap[i] = false;
+
+	window.onkeydown = this.handlerDown.bind(this);
+	window.onkeyup = this.handlerUp.bind(this);
+}
+
+Planets.Keymap.prototype.handlerDown = function(event) {
+	event.preventDefault();
+	this.keymap[event.keyCode] = true;
+}
+
+Planets.Keymap.prototype.handlerUp = function(event) {
+	event.preventDefault();
+	this.keymap[event.keyCode] = false;
+}
---
+++ js/planet-util.js
@@ -0,0 +1,276 @@
+var distance  = function(position1, position2) {
+	var x2 = position2.x,
+		y2 = position2.y;
+	return Math.sqrt((x2 -= position1.x) * x2 + (y2 -= position1.y) * y2);
+}
+
+var angulate = function(position1, position2) {
+	var a = Math.atan2(-(position2.y-position1.y), (position2.x-position1.x)) * (1024/PI2);
+	return 1024 - ((a < 0)? a + 1024 : a);
+}
+
+Array.prototype.init = function(value) {
+	for(var i = 0; i < this.length; i++)
+		this[i] = value;
+	return this;
+}
+
+Array.prototype.random = function() {
+	return this[(Math.random() * this.length) | 0];
+}
+
+var Store = {
+
+	uidIndex : 0,
+
+	//Call with number parameter to get an array
+	//of stores with the parameter as length.
+	//Call with object as parameter to extend
+	//that object
+	create : function(objOrCount) { 
+
+		if(typeof objOrCount == "number") {
+			var ar = new Array(objOrCount);
+			for(var i = 0; i < objOrCount; i++)
+				ar[i] = Store.create();
+			return ar;
+		}
+
+		var self = this;
+
+		return $.extend(((typeof obj == "object")? obj : {}), {
+			slots : [],
+			uid : self.uidIndex++,
+			length : 0,
+			size : 0,
+
+			add : function(element) {
+				if(this.slots.length > 0) {
+					var pi = this.slots.pop();
+					if(!element.storeIndex) element.storeIndex = [];
+					element.storeIndex[this.uid] = pi;
+					this[pi] = element;
+				} else {
+					if(!element.storeIndex) element.storeIndex = [];
+					element.storeIndex[this.uid] = this.length;
+					this[this.length] = element;
+					this.length++;
+				}
+
+				this.size++;
+				return element.storeIndex[this.uid];
+			},
+
+			remove : function(element) {
+				var pi;
+				if(!element.storeIndex || !(pi = element.storeIndex[this.uid])) {
+					console.log("Store element never added to store?");
+					return;
+				}
+
+				element.storeIndex[this.uid] = -1;
+				this[pi] = null;
+				this.slots.push(pi);
+
+				this.size--;
+			},
+
+			contains : function(element) {
+				return !!(element.storeIndex && 
+						this.uid in element.storeIndex &&
+						element.storeIndex[this.uid] != -1);
+			},
+
+			count : function() {
+				return this.size;
+			},
+
+			each : function(callback) {
+				if(!this.size) return;
+				var args = Array.prototype.slice.call(arguments);
+				var call = args.shift();
+
+				for(var i = 0; i < this.length; i++) {
+					if(this[i] == null) continue;
+					call.apply(this[i], args);
+				}
+			},
+
+			//Calls callback "times" times on different
+			//objects
+			times : function(callback, times) {
+				var times = Math.min(this.size, times);
+				if(times == 0) return;
+
+				for(var i = 0; i < this.length; i++) {
+					if(times-- <= 0) return;
+					callback.apply(this[i], []);
+				}
+			},
+
+			eachApply : function(callback, args) {
+				if(!this.size) return;
+				for(var i = 0; i < this.length; i++) {
+					if(this[i] == null) continue;
+					callback.apply(this[i], args);
+				}
+			},
+
+			empty : function() {
+				return !this.size;
+			},
+
+			random : function() {
+				if(!this.size) return null;
+				var seed = (Math.random() * this.length) | 0;
+				while(true) {
+					if(this[seed])
+						return this[seed];
+					if(++seed >= this.length)
+						seed = 0;
+				}
+			}
+		});
+
+	}
+
+};
+
+var RenderLayer = {
+
+	create : function(game, context) {
+		var self = this;
+		var store = Store.create();
+		return $.extend(Store.create(), {
+			invalid : true,
+
+			fnUpdate : {},
+			fnRender : {},
+
+			invalidate : function() {
+				this.invalid = true;
+				return this;
+			},
+
+			isInvalid : function() {
+				return this.invalid;
+			},
+
+			update : function() {
+				if(!this.invalid) return this;
+				var args = arguments, self = this;
+
+				this.each(function() {
+					var fn = self.fnUpdate[this.storeIndex[self.uid]];
+					this[fn].apply(this, args);
+				});
+
+				return this;
+			},
+
+			render : function() {
+				if(!this.invalid) return this;
+				var args = arguments, self = this;
+
+				this.each(function() {
+					var fn = self.fnRender[this.storeIndex[self.uid]];
+					this[fn].apply(this, args);
+				});
+
+				this.invalid = false;
+				return this;
+			},
+
+			include : function(element, updateFn, renderFn) {
+				var i = this.add(element);
+				if(!updateFn) var updateFn = "update";
+				if(!renderFn) var renderFn = "render";
+				this.fnUpdate[i] = updateFn;
+				this.fnRender[i] = renderFn;
+
+				return this;
+			}
+
+		});
+	}
+};
+
+
+var DebugOutput = {
+	fpsTimer : {
+		fpsTimer : Date.now(),
+		renderTime : 0,
+		updateTime : 0,
+		updateCounter : 0,
+
+		updateStartTimer : 0,
+		renderStartTimer : 0,
+
+		lastUpdateFPS : 0,
+		lastRenderFPS : 0,
+		lastFullFPS : 0,
+
+		potentialFPS : 0,
+
+		fps : 0,
+
+		updateStart : function() { 
+			this.updateStartTimer = Date.now();
+		},
+		updateEnd : function() { 
+			this.updateTime += (Date.now() - this.updateStartTimer);
+		},
+		renderStart : function() { 
+			this.renderStartTimer = Date.now();
+		},
+		renderEnd : function() { 
+			this.renderTime += Date.now() - this.renderStartTimer;
+			this.updateCounter++;
+		},
+
+		show : function() { 
+			if(Date.now() - this.fpsTimer > 1000) {
+				this.fpsTimer = Date.now();
+				this.lastUpdateFPS = Math.round(this.updateTime / this.updateCounter);
+				this.lastRenderFPS = Math.round(this.renderTime / this.updateCounter);
+				this.lastFullFPS = Math.round((this.renderTime + this.updateTime) / this.updateCounter);
+				this.fps = this.updateCounter;
+				this.updateCounter = 0;
+				this.updateTime = this.renderTime = 0;
+				this.potentialFPS = Math.round(1000 / this.lastFullFPS);
+			}
+
+			DebugOutput.write("Update", this.lastUpdateFPS);
+			DebugOutput.write("Render", this.lastRenderFPS);
+			DebugOutput.write("Combined", this.lastFullFPS);
+			DebugOutput.write("fps (c)", this.fps);
+			DebugOutput.write("fps (f)", this.potentialFPS);
+		}
+	},
+
+	write : function(name, value) {
+		this.towriteName.push("" + name);
+		this.towriteValue.push("" + value);
+	},
+
+	towriteName : [],
+	towriteValue: [],
+
+	flush : function(viewport, context) { 
+		if(this.towriteName.length == 0) return;
+		var yoffset = 20 - viewport.offset.y;
+		var x1 = 10 - viewport.offset.x,
+			x2 = 90 - viewport.offset.x;
+
+		context.font = "10pt Lucida Console";
+		context.fillStyle = "#ffffff";
+		for(var i = 0; i < this.towriteName.length; i++) {
+			context.fillText(this.towriteName[i],  x1, yoffset + (15 * i));
+			context.fillText(this.towriteValue[i], x2, yoffset + (15 * i));
+		}
+		context.fill();
+
+		this.towriteName = [];
+		this.towriteValue = [];
+	}
+}
